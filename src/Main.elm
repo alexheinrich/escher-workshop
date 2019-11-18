088b1a78999ae19069adb685460e5f8c4c113c11
Alexander Heinrich <alex.heinrich@icloud.com>
2019-11-18 22:14:44 +0100

    Add ranges for all points

--- src/Main.elm
+++ src/Main.elm
@@ -15,22 +15,42 @@ import Svg exposing (Svg)
 
 
 type Msg
-    = UpdateAx String
+    = Update ABC Direction String
+
+
+type Direction
+    = X
+    | Y
+
+
+type ABC
+    = A
+    | B
+    | C
+
+
+type alias Point =
+    { x : Float, y : Float }
 
 
 type alias Model =
-    { x : Float, y : Float }
+    { a : Point, b : Point, c : Point }
 
 
 placeInsideDiv : Model -> Svg Msg -> Html Msg
 placeInsideDiv model svg =
-    div [ style "padding" "50px" ] [ svg, range { min = 0, max = 300, value = model.x } ]
+    div [ style "padding" "50px" ]
+        [ svg
+        , rangeXY { label = "A", model = model.a, msg = Update A }
+        , rangeXY { label = "B", model = model.b, msg = Update B }
+        , rangeXY { label = "C", model = model.c, msg = Update C }
+        ]
 
 
 main : Program () Model Msg
 main =
     Browser.sandbox
-        { init = { x = 125.0, y = 75.0 }
+        { init = { a = { x = 125.0, y = 75.0 }, b = { x = 250.0, y = 0.0 }, c = { x = 0.0, y = 250.0 } }
         , view = view
         , update = update
         }
@@ -40,9 +60,9 @@ view : Model -> Html Msg
 view model =
     let
         box =
-            { a = model
-            , b = { x = 250.0, y = 0.0 }
-            , c = { x = 0.0, y = 250.0 }
+            { a = model.a
+            , b = model.b
+            , c = model.c
             }
     in
     box
@@ -52,25 +72,53 @@ view model =
 
 
 update : Msg -> Model -> Model
-update (UpdateAx newAx) model =
+update (Update abc direction newVal) model =
     let
-        newX =
-            String.toFloat newAx
-                |> Maybe.withDefault model.x
+        new =
+            String.toFloat newVal
+                |> Maybe.withDefault 0
     in
-    { model | x = newX }
+    case abc of
+        A ->
+            { model | a = updatePoint model.a direction new }
+
+        B ->
+            { model | b = updatePoint model.b direction new }
+
+        C ->
+            { model | c = updatePoint model.c direction new }
 
 
-range : { min : Int, max : Int, value : Float } -> Html Msg
-range { min, max, value } =
+updatePoint : Point -> Direction -> Float -> Point
+updatePoint point direction new =
+    case direction of
+        X ->
+            { point | x = new }
+
+        Y ->
+            { point | y = new }
+
+
+range : { min : Int, max : Int, value : Float, label : String, msg : String -> Msg } -> Html Msg
+range { min, max, value, label, msg } =
     div []
-        [ input
+        [ text label
+        , input
             [ type_ "range"
             , Html.Attributes.min (String.fromInt min)
             , Html.Attributes.max (String.fromInt max)
             , Html.Attributes.value (String.fromFloat value)
-            , Html.Events.onInput UpdateAx
+            , Html.Events.onInput msg
             ]
             []
         , text (String.fromFloat value)
+        ]
+
+
+rangeXY : { label : String, model : Point, msg : Direction -> String -> Msg } -> Html Msg
+rangeXY { label, model, msg } =
+    div []
+        [ text label
+        , range { min = -1000, max = 1000, value = model.x, label = "X", msg = msg X }
+        , range { min = -1000, max = 1000, value = model.y, label = "Y", msg = msg Y }
         ]
